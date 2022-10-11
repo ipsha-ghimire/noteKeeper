95118169db4254666bd22a7c6c3dd9a2d406ea7d
Ipsha Ghimire <ipshagh2000@gmail.com>
2022-10-11 13:25:41 +0545

    static website formed

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.render('index',{title:'ipsha'});
+});
+
+router.get('/home',function(req,res,next){
+  res.render('home',{
+    firstname:'ipsha',
+    lastname:'ghimire',
+    age:22,
+    study:'bachelors'
+  });
+})
+module.exports = router;
